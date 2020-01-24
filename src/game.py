2c1d8846682e897679d84b74d460a9a6f8c34328
rachelndlc <58004269+rachelndlc@users.noreply.github.com>
2020-01-24 11:47:41 -0500

    Update game.py

--- src/game.py
+++ src/game.py
@@ -34,7 +34,7 @@ TotalGrid = []
 totxsize = ((WIDTH + MARGIN) * amtrow)+5*MARGIN
 totysize = ((HEIGHT + MARGIN) * amtcol)+5*MARGIN
 
-#formatting **********************************
+#formatting to create the major box
 def ResizePlayBox(playrect,allowedx,allowedy):
     if allowedx == -1 and allowedy == -1:
         playrect[0] = MARGIN
@@ -51,6 +51,8 @@ def ResizePlayBox(playrect,allowedx,allowedy):
 #determining next box in game play
 def NextBox(Ccords,TotalGrid):
     boxFull = False
+    #ccords are the individual small squares
+    #here they are divided up
     if TotalGrid[Ccords[0]%3][Ccords[1]%3] != 0:
         return (-1,-1)
     else:
@@ -97,3 +99,100 @@ def winSetter(OC,Team,Grid,TotalGrid):
         else:
             SetWin(Grid,2,TotalGrid)
     print(TotalGrid)
+
+def winCalc(OuterCords,Grid,TotalGrid):
+    #the major outside boxes
+    OC = OuterCords
+    #the small inner boxes
+    IC = (OC[0]%3,OC[1]%3)
+    thisTileTeam = Grid[OC[0]][OC[1]]
+    Team = thisTileTeam
+    if thisTileTeam == 0:
+        return "Invaild Team"
+    if IC == (0,0):
+        #determines if the win is made in one of the major outside boxes
+        #it will execute the defined winSetter above
+        if Grid[OC[0]+1][OC[1]] == thisTileTeam and Grid[OC[0]+2][OC[1]] == thisTileTeam:
+            winSetter(OC,thisTileTeam,Grid,TotalGrid)
+        elif Grid[OC[0]][OC[1]+1] == thisTileTeam and Grid[OC[0]][OC[1]+2] == thisTileTeam:
+            winSetter(OC,thisTileTeam,Grid,TotalGrid)
+        elif Grid[OC[0]+1][OC[1]+1] == thisTileTeam and Grid[OC[0]+2][OC[1]+2] == thisTileTeam:
+            winSetter(OC,thisTileTeam,Grid,TotalGrid)
+    elif IC == (1,0):
+        #similar to what's above except this is in reference to the small boxes
+        if Grid[OC[0]+1][OC[1]] == thisTileTeam and Grid[OC[0]-1][OC[1]] == thisTileTeam:
+            winSetter(OC,thisTileTeam,Grid,TotalGrid)
+        elif Grid[OC[0]][OC[1]+1] == thisTileTeam and Grid[OC[0]][OC[1]+2] == thisTileTeam:
+            winSetter(OC,thisTileTeam,Grid,TotalGrid)
+    elif IC == (2,0):
+        if Grid[OC[0]-1][OC[1]] == thisTileTeam and Grid[OC[0]-2][OC[1]] == thisTileTeam:
+            winSetter(OC,thisTileTeam,Grid,TotalGrid)
+        elif Grid[OC[0]][OC[1]+1] == thisTileTeam and Grid[OC[0]][OC[1]+2] == thisTileTeam:
+            winSetter(OC,thisTileTeam,Grid,TotalGrid)
+        elif Grid[OC[0]-1][OC[1]+1] == thisTileTeam and Grid[OC[0]-2][OC[1]+2] == thisTileTeam:
+            winSetter(OC,thisTileTeam,Grid,TotalGrid)
+    elif IC == (0,1):
+        if Grid[OC[0]][OC[1]+1] == thisTileTeam and Grid[OC[0]][OC[1]-1] == thisTileTeam:
+            winSetter(OC,thisTileTeam,Grid,TotalGrid)
+        elif Grid[OC[0]+1][OC[1]] == thisTileTeam and Grid[OC[0]+2][OC[1]] == thisTileTeam:
+            winSetter(OC,thisTileTeam,Grid,TotalGrid)
+    elif IC == (1,1):
+        if Grid[OC[0]][OC[1]+1] == thisTileTeam and Grid[OC[0]][OC[1]-1] == thisTileTeam:
+            winSetter(OC,thisTileTeam,Grid,TotalGrid)
+        elif Grid[OC[0]+1][OC[1]] == thisTileTeam and Grid[OC[0]-1][OC[1]] == thisTileTeam:
+            winSetter(OC,thisTileTeam,Grid,TotalGrid)
+        elif Grid[OC[0]-1][OC[1]-1] == thisTileTeam and Grid[OC[0]+1][OC[1]+1] == thisTileTeam:
+            winSetter(OC,thisTileTeam,Grid,TotalGrid)
+        elif Grid[OC[0]-1][OC[1]+1] == thisTileTeam and Grid[OC[0]+1][OC[1]-1] == thisTileTeam:
+            winSetter(OC,thisTileTeam,Grid,TotalGrid)
+
+    elif IC == (2,1):
+        if Grid[OC[0]][OC[1]+1] == thisTileTeam and Grid[OC[0]][OC[1]-1] == thisTileTeam:
+            winSetter(OC,thisTileTeam,Grid,TotalGrid)
+        elif Grid[OC[0]-1][OC[1]] == thisTileTeam and Grid[OC[0]-2][OC[1]] == thisTileTeam:
+            winSetter(OC,thisTileTeam,Grid,TotalGrid)
+
+    elif IC == (0,2):
+        if Grid[OC[0]+1][OC[1]] == thisTileTeam and Grid[OC[0]+2][OC[1]] == thisTileTeam:
+            winSetter(OC,thisTileTeam,Grid,TotalGrid)
+        elif Grid[OC[0]][OC[1]-1] == thisTileTeam and Grid[OC[0]][OC[1]-2] == thisTileTeam:
+            winSetter(OC,thisTileTeam,Grid,TotalGrid)
+        elif Grid[OC[0]+1][OC[1]-1] == thisTileTeam and Grid[OC[0]+2][OC[1]-2] == thisTileTeam:
+            winSetter(OC,thisTileTeam,Grid,TotalGrid)
+    elif IC == (1,2):
+        if Grid[OC[0]+1][OC[1]] == thisTileTeam and Grid[OC[0]-1][OC[1]] == thisTileTeam:
+            winSetter(OC,thisTileTeam,Grid,TotalGrid)
+        elif Grid[OC[0]][OC[1]-1] == thisTileTeam and Grid[OC[0]][OC[1]-2] == thisTileTeam:
+            winSetter(OC,thisTileTeam,Grid,TotalGrid)
+
+    elif IC == (2,2):
+        if (Grid[OC[0]][OC[1]-1] == thisTileTeam and Grid[OC[0]][OC[1]-2] == thisTileTeam) :
+            winSetter(OC,thisTileTeam,Grid,TotalGrid)
+        elif (Grid[OC[0]-1][OC[1]] == thisTileTeam and Grid[OC[0]-2][OC[1]] == thisTileTeam):
+            winSetter(OC,thisTileTeam,Grid,TotalGrid)
+        elif Grid[OC[0]-1][OC[1]-1] == thisTileTeam and Grid[OC[0]-2][OC[1]-2] == thisTileTeam:
+            winSetter(OC,thisTileTeam,Grid,TotalGrid)
+    amtused = 0
+    #print(OC[0],IC[0],OC[1],IC[1])
+    for x in range(OC[0]-IC[0],OC[0]-IC[0]+3):
+        for y in range(OC[1]-IC[1],OC[1]-IC[1]+3):
+            if Grid[x][y] != 0:
+                amtused = amtused + 1
+    if amtused == 9:
+        if TotalGrid[OC[0]//3][OC[1]//3] == 0:
+            TotalGrid[OC[0]//3][OC[1]//3] = -1
+            print("Box tied!!")
+    #print(TiedBox)
+for row in range(3):
+
+    TotalGrid.append([])
+    for column in range(3):
+        TotalGrid[row].append(0)
+for row in range(amtrow):
+
+    grid.append([])
+    for column in range(amtcol):
+        grid[row].append(0)
+
+#prints game
+print(grid)
